f209dc8281183988b72f4acb233af92fbcbdec10
Enrico <enricofen300@gmail.com>
2024-04-22 11:52:10 +0200

    refactor

--- app/src/main/java/com/example/personalphysicaltracker/MainActivity.kt
+++ app/src/main/java/com/example/personalphysicaltracker/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_charts, R.id.nav_info
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
